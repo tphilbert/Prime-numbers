02ae1c30cb4d79ce1c5619dc682becea612a5a55
Philbert <Philbert@DESKTOP-VGA2PAV>
2022-06-11 17:47:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace primenumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number up to which you want to list primes from 1");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+            bool[] prime = new bool[n + 1];
+
+
+
+            for (int i = 2; i <= n; i++) prime[i] = true;
+
+            int limit = (int)Math.Ceiling(Math.Sqrt(n));
+
+
+
+            for (int i = 2; i <= limit; i++)
+                if (prime[i])
+                    for (int j = i * i; j <= n; j += i)
+                        prime[j] = false;
+
+
+
+            Console.WriteLine("Prime Numbers up to {0}", n);
+
+
+
+            for (int i = 0; i <= n; i++)
+                if (prime[i])
+                    Console.WriteLine(i);
+        }
+    }
+}
